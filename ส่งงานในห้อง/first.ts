cecd403d419e5a8fb8a23d91015b8d88d935780c
Deeprom Phiwoon <bbbbbbbbe53@gmail.com>
2026-06-17 11:41:01 +0700

    ส่งงานในห้อง

---
+++ ส่งงานในห้อง/first.ts
@@ -0,0 +1,19 @@
+let fname: string = "Deeprom";
+let lname: string = "Phiwoon";
+let old: number = 19;
+let dream: string = "Programmer"
+let course: string = "Object-oriented Programing";
+let mark: number = 80;
+let pass: boolean = true;
+
+
+console.log("Name: %s %s \nOld: %d Year old \nDream Job: %s \nCouse: %s \nMark: %d",fname,lname,old,dream,course,mark);
+console.log("Pass:",pass);
+let courses: string[] = ["Database", "Programming", "Web"];
+let marks: number[] = [75,72,81];
+console.log(marks);
+console.log("Mark1: %d",marks[1]);
+console.log(courses);
+console.log("Courses: %s",courses[1]);
+console.log("Total Marks: %d",marks[0]+marks[1]+marks[2]);
+console.log("Total Marks Average: %d",(marks[0]+marks[1]+marks[2])/3);
